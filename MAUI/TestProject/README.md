07a42b576333026095a1f225c2a6bd47a1934c21
Stefan Sinapov <stefan.sinapov@gmail.com>
2023-07-19 18:52:44 +0300

    add README for MAUI

---
+++ MAUI/TestProject/README.md
@@ -0,0 +1,6 @@
+
+## Android emulator
+![android emulator](./maui_android.gif)
+
+## iOS Emulator
+![ios emulator](./maui_ios.gif)
--- MAUI/TestProject/TestProject.Mobile/Views/MapPage.xaml.cs
+++ MAUI/TestProject/TestProject.Mobile/Views/MapPage.xaml.cs
@@ -8,26 +8,20 @@ namespace TestProject.Mobile.Views;
 public partial class MapPage : ContentPage
 {
     private CancellationTokenSource _cancelTokenSource;
-    private bool _isCheckingLocation;
     private Location _officeLocation = new Location(42.683381890105764, 23.35473960346132);
 
     public MapPage()
     {
         InitializeComponent();
-        this.ClearSelectionButton.IsVisible = false;
         BindingContext = new PinItemsSourcePageViewModel();
         map.MoveToRegion(MapSpan.FromCenterAndRadius(_officeLocation, Distance.FromKilometers(10)));
-    }
-
-
-    void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
-    {
-        double zoomLevel = e.NewValue;
-        double latlongDegrees = 360 / (Math.Pow(2, zoomLevel));
-        if (map.VisibleRegion != null)
+        map.Pins.Add(new Pin
         {
-            map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongDegrees, latlongDegrees));
-        }
+            Type = PinType.Place,
+            Label = "DextraSoft",
+            Address = "Sofia, Bulgaria",
+            Location = _officeLocation
+        });
     }
 
     void OnButtonClicked(object sender, EventArgs e)
@@ -51,32 +45,25 @@ public partial class MapPage : ContentPage
     {
         Debug.WriteLine($"MapClick: {e.Location.Latitude}, {e.Location.Longitude}");
 
-        if (map.Pins.Count(p => p.Type == PinType.Generic) == 0)
+        var pin = new Pin
         {
-            var pin = new Pin
-            {
-                Type = PinType.Generic,
-                Location = e.Location,
-                Label = "Pin",
-                Address = "Pin Address"
-            };
-            map.Pins.Add(pin);
-            this.ClearSelectionButton.IsVisible = true;
-        }
+            Type = PinType.Generic,
+            Location = e.Location,
+            Label = "Pin",
+            Address = $"Pin Address {e.Location.Latitude}, {e.Location.Longitude}"
+        };
+        map.Pins.Add(pin);
     }
 
-    private void ClearSelectionButtonClicked(object sender, EventArgs e)
+    void OnGoToOffice(object sender, EventArgs e)
     {
-        this.ClearSelectionButton.IsVisible = false;
-        map.Pins.Clear();
+        map.MoveToRegion(MapSpan.FromCenterAndRadius(_officeLocation, Distance.FromMeters(200)));
     }
 
     private async void GetCurrentLocationButton_Clicked(object sender, EventArgs e)
     {
         try
         {
-            _isCheckingLocation = true;
-
             GeolocationRequest request = new(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
 
             _cancelTokenSource = new CancellationTokenSource();
@@ -93,6 +80,8 @@ public partial class MapPage : ContentPage
                     Label = "Current Location",
                     Location = location
                 });
+
+                map.MoveToRegion(mapSpan: MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(1)));
             }
         }
         // Catch one of the following exceptions:
@@ -104,29 +93,8 @@ public partial class MapPage : ContentPage
             // Unable to get location
             Debug.WriteLine($"Unable to get location: {ex.Message}");
         }
-        finally
-        {
-            _isCheckingLocation = false;
-        }
     }
 
-    private async void LauchMapApp(object sender, EventArgs e)
-    {
-        if (DeviceInfo.Current.Platform == DevicePlatform.iOS || DeviceInfo.Current.Platform == DevicePlatform.MacCatalyst)
-        {
-            // https://developer.apple.com/library/ios/featuredarticles/iPhoneURLScheme_Reference/MapLinks/MapLinks.html
-            await Launcher.OpenAsync("http://maps.apple.com/?q=394+Pacific+Ave+San+Francisco+CA");
-        }
-        else if (DeviceInfo.Current.Platform == DevicePlatform.Android)
-        {
-            // opens the Maps app directly
-            await Launcher.OpenAsync("geo:0,0?q=394+Pacific+Ave+San+Francisco+CA");
-        }
-        else if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
-        {
-            await Launcher.OpenAsync("bingmaps:?where=394 Pacific Ave San Francisco CA");
-        }
-    }
 
     private async void OpenMapApp(object sender, EventArgs e)
     {
