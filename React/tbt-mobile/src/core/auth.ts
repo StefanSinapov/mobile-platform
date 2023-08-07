ad849edaf81c1fbe264eae2b0fe11bf520183840
Stefan Sinapov <stefan.sinapov@gmail.com>
2023-08-07 16:04:35 +0300

    add some tests

--- React/tbt-mobile/src/core/auth.ts
+++ React/tbt-mobile/src/core/auth.ts
@@ -156,4 +156,3 @@ function fakeCheckAuth() {
 function fakeIsAuthError(error: any) {
   return [401, 403].includes(error?.errorCode);
 }
-
--- React/tbt-mobile/src/utils/storage/storage.test.ts
+++ React/tbt-mobile/src/utils/storage/storage.test.ts
@@ -5,28 +5,92 @@ import { load, loadString, save, saveString, clear, remove } from "./storage"
 const VALUE_OBJECT = { x: 1 }
 const VALUE_STRING = JSON.stringify(VALUE_OBJECT)
 
-beforeEach(() => (AsyncStorage.getItem as jest.Mock).mockReturnValue(Promise.resolve(VALUE_STRING)))
+// beforeEach(() => (AsyncStorage.getItem as jest.Mock).mockReturnValue(Promise.resolve(VALUE_STRING)))
 afterEach(() => jest.clearAllMocks())
 
-test("load", async () => {
-  const value = await load("something")
-  expect(value).toEqual(JSON.parse(VALUE_STRING))
-})
+function mockGetItem(result: string | null) {
+  (AsyncStorage.getItem as jest.Mock).mockResolvedValueOnce(Promise.resolve(result))
+}
 
-test("loadString", async () => {
-  const value = await loadString("something")
-  expect(value).toEqual(VALUE_STRING)
-})
+describe('loadString', () => {
+  it('should return the value from storage', async () => {
+    const value = 'some value';
+    mockGetItem(VALUE_STRING)
+    const result = await loadString('SOME_KEY');
+    expect(result).toBe(VALUE_STRING);
+  });
 
-test("save", async () => {
-  await save("something", VALUE_OBJECT)
-  expect(AsyncStorage.setItem).toHaveBeenCalledWith("something", VALUE_STRING)
-})
+  it('should return null if the value is not found in storage', async () => {
+    mockGetItem(null);
+    const result = await loadString('SOME_KEY');
+    expect(result).toBeNull();
+  });
 
-test("saveString", async () => {
-  await saveString("something", VALUE_STRING)
-  expect(AsyncStorage.setItem).toHaveBeenCalledWith("something", VALUE_STRING)
-})
+  it('should return null if there is an error loading the value from storage', async () => {
+    const error = new Error('Some error occurred');
+    (AsyncStorage.getItem as jest.Mock).mockRejectedValueOnce(error);
+    const result = await loadString('SOME_KEY');
+    expect(result).toBeNull();
+  });
+});
+
+
+describe('load', () => {
+  it('should return the parsed value from storage', async () => {
+    const value = { someKey: 'some value' };
+    mockGetItem(JSON.stringify(value));
+    const result = await load('SOME_KEY');
+    expect(result).toEqual(value);
+  });
+
+  it('should return null if the value is not found in storage', async () => {
+    mockGetItem(null);
+    const result = await load('SOME_KEY');
+    expect(result).toBeNull();
+  });
+
+  it('should return null if there is an error loading the value from storage', async () => {
+    const error = new Error('Some error occurred');
+    (AsyncStorage.getItem as jest.Mock).mockRejectedValueOnce(error);
+    const result = await load('SOME_KEY');
+    expect(result).toBeNull();
+  });
+
+  it('should log an error if there is an error parsing the value from storage', async () => {
+    mockGetItem('invalid json');
+    const result = await load('SOME_KEY');
+    expect(result).toBeNull();
+  });
+});
+
+
+describe('saveString', () => {
+  it('should save the value to storage', async () => {
+    const result = await saveString("something", VALUE_STRING)
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith("something", VALUE_STRING)
+    expect(result).toBe(true);
+  });
+
+  it('should return false if there is an error saving the value to storage', async () => {
+    (AsyncStorage.setItem as jest.Mock).mockRejectedValueOnce(new Error('Some error occurred'));
+    const result = await saveString("something", VALUE_STRING);
+    expect(result).toBe(false);
+  });
+});
+
+describe('save', () => {
+  it('should save the parsed value to storage', async () => {
+    const result = await save("something", VALUE_OBJECT)
+    expect(result).toBe(true);
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith("something", VALUE_STRING)
+  });
+
+  it('should return false if there is an error saving the parsed value to storage', async () => {
+    (AsyncStorage.setItem as jest.Mock).mockRejectedValueOnce(new Error('Some error occurred'));
+    const result = await save("something", VALUE_OBJECT)
+    expect(result).toBe(false);
+  });
+});
 
 test("remove", async () => {
   await remove("something")
@@ -37,3 +101,4 @@ test("clear", async () => {
   await clear()
   expect(AsyncStorage.clear).toHaveBeenCalledWith()
 })
+
